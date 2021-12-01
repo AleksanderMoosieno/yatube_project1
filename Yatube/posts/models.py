e54ac0815768d2ec45b9a9a84c5f4d9f1b389f8a
Александр Мусиенко <AleksanderMoosienko@yandex.ru>
2021-12-02 00:45:33 +0300

    zzs

--- Yatube/posts/models.py
+++ Yatube/posts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Group(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=200)
     slug = models.CharField(max_length=50)
     description = models.TextField(blank=True, null=True)
 
--- Yatube/posts/views.py
+++ Yatube/posts/views.py
@@ -17,15 +17,9 @@ def index(request):
 
 
 def group_posts(request, slug):
-    # Функция get_object_or_404 получает по заданным критериям объект 
-    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
-    # В нашем случае в переменную group будут переданы объекты модели Group,
-    # поле slug у которых соответствует значению slug в запросе
     template = 'posts /group_list.html'
-
-    errorMsg = get_object_or_404(Group, slug = slug )
+    group = get_object_or_404(Group, slug=slug)
     title = Group.__str__
-    posts = Post.objects.filter(group_id = slug)[:10]
     # Метод .filter позволяет ограничить поиск по критериям.
     # Это аналог добавления
     # условия WHERE group_id = {group_id}
@@ -35,5 +29,7 @@ def group_posts(request, slug):
         'title': title,
     }
     return render(request, template, context)
+
+
 
  
